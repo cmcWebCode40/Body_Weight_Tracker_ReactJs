c84dc0c82e77fad396e5b5c600776297a240a15c
cmcWebCode40 <infinity_michael@yahoo.com>
2020-01-20 13:15:43 +0100

    added open sans google fonts

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle` 
      *,
@@ -15,25 +15,25 @@ export default createGlobalStyle`
 
      html  {
           font-size : 62.5%;
-          --color-primary: ${ props => props.theme.colors.primary};
-          --color-bodyColor: ${ props => props.theme.colors.bodyColor};
-          --color-bodyDark: ${ props => props.theme.colors.bodyDark};
-          --color-bodyLight: ${ props => props.theme.colors.bodyLight};
-          --color-text: ${ props => props.theme.colors.textColor};
-          --color-white: ${ props => props.theme.colors.whiteColor};
-          --color-shadowColor: ${ props => props.theme.colors.shadowColor};
+          --color-primary: ${props => props.theme.colors.primary};
+          --color-bodyColor: ${props => props.theme.colors.bodyColor};
+          --color-bodyDark: ${props => props.theme.colors.bodyDark};
+          --color-bodyLight: ${props => props.theme.colors.bodyLight};
+          --color-text: ${props => props.theme.colors.textColor};
+          --color-white: ${props => props.theme.colors.whiteColor};
+          --color-shadowColor: ${props => props.theme.colors.shadowColor};
 
-          @media ${ props => props.theme.mediaQueries.smallest} {
+          @media ${props => props.theme.mediaQueries.smallest} {
                font-size : 60%;
           }
 
-          @media ${ props => props.theme.mediaQueries.small} {
+          @media ${props => props.theme.mediaQueries.small} {
                font-size : 55%; 
           }
      }
 
      body {
-          font-family: sans-serif;
+          font-family: 'Open Sans', sans-serif;
           font-weight: 400;
           line-height :  1.6;
      }
@@ -50,16 +50,16 @@ export default createGlobalStyle`
      }
 
      ::-webkit-scrollbar-track {
-          background-color:${ props => props.theme.colors.bodyDark} ;
+          background-color:${props => props.theme.colors.bodyDark} ;
      }
 
      ::-webkit-scrollbar-thumb {
-          background-color:${ props => props.theme.colors.primary};
+          background-color:${props => props.theme.colors.primary};
      }
 
      ::-webkit-scrollbar-thumb:hover {
-          background-color:${ props => props.theme.colors.primary};
+          background-color:${props => props.theme.colors.primary};
      }
 
 
-`
+`;
